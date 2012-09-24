2e55f7ebcaa0ea5889476153e1ff1cc2b25249d8
Michael Laurenzano <michaell@sajak.(none)>
2012-09-24 00:37:28 -0700

    Still better testing. Added some files to comply with github recommendations.

--- test/test.cpp
+++ test/test.cpp
@@ -20,48 +20,72 @@
 
 #include <ReuseDistance.hpp>
 
+using namespace std;
+
 #define TEST_WINDOW_SIZE (100)
+#define SEPERATOR "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 
 int main(int argc, char* argv[]){
-    ReuseDistance* r = new ReuseDistance(0);
+    ReuseDistance* r1 = new ReuseDistance(0);
+    ReuseDistance* r2 = new ReuseDistance(10);
+    ReuseDistance* r3 = new ReuseDistance(100);
+    ReuseDistance* r4 = new ReuseDistance(1000);
     ReuseEntry entry = ReuseEntry();
     entry.id = 0;
 
     for (uint32_t i = 0; i < 3; i++){
         for (uint32_t j = 0; j < TEST_WINDOW_SIZE; j++){
             entry.address = j;
-            r->Process(entry);
+            r1->Process(entry);
+            r2->Process(entry);
+            r3->Process(entry);
+            r4->Process(entry);
         }
     }
 
-    r->Print();
+    r1->Print();
+    r2->Print();
+    r3->Print();
+    r4->Print();
+    cout << SEPERATOR;
 
     for (uint32_t i = 0; i < 3; i++){
         for (uint32_t j = 0; j < TEST_WINDOW_SIZE - 1; j++){
             entry.address = j;
-            r->Process(entry);
+            r1->Process(entry);
+            r2->Process(entry);
+            r3->Process(entry);
+            r4->Process(entry);
         }
     }
 
-    r->Print();
+    r1->Print();
+    r2->Print();
+    r3->Print();
+    r4->Print();
+    cout << SEPERATOR;
 
     for (uint32_t i = 0; i < 3; i++){
         for (uint32_t j = 0; j < TEST_WINDOW_SIZE - 1; j += 2){
             entry.address = j;
-            r->Process(entry);
+            r1->Process(entry);
+            r2->Process(entry);
+            r3->Process(entry);
+            r4->Process(entry);
         }
     }
 
-    r->Print();
+    r1->Print();
+    r2->Print();
+    r3->Print();
+    r4->Print();
+    cout << SEPERATOR;
 
-    for (uint32_t i = 0; i < 3; i++){
-        for (uint32_t j = 0; j < TEST_WINDOW_SIZE - 1; j += 4){
-            entry.address = j;
-            r->Process(entry);
-        }
-    }
-
-    r->Print();
+    ReuseDistance* r5 = new ReuseDistance(*r1);
+    r1->Print();
+    // TODO: r5 != r1
+    r5->Print();
+    cout << SEPERATOR;
 
     return 0;
 }
